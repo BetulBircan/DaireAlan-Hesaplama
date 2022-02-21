09e1161e1b09e6c8ad4293d3df3adea61bb6ab2c
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-02-21 22:14:29 +0300

    dairenin alnını hesaplamak için yaricap ve piSayisi değişkenleri tanımlandı

--- daire.js
+++ daire.js
@@ -2,5 +2,10 @@
 Konsol çıktısı: Yarıçapı (Yarıçap) olan dairenin alanı: (Alan) şeklinde olmalıdır. */
 
 //Process nesnesi node.js e ait olan global nesnelerden bir tanesidir ve bize o anki işlemle ilgili  bilgi verir.
-//Process in argv özelliği bize bir tane array döndü ve bu array programı çalıştırırken kullandığımız parametrelerdir.
-//
+//Process in argv özelliği bize array olarak döner ve bu array programı çalıştırırken kullandığımız parametrelerdir.
+//process.argv.slice(2) diyerek ilk iki parametreyi çıkartıyoruz.
+
+//Şimdi de dairenin alanı için yarıçap ve piSayısı değişkenlerini tanımlıyorum
+let arguments = process.argv.slice(2);
+let yaricap = arguments[0]*1
+const piSayisi = 3.14;
