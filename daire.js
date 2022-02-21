054f1ed5a9d7b30d0e638af7049a92929db128ca
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-02-21 20:01:50 +0300

    daire.js dosyası oluşturuldu

---
+++ daire.js
@@ -0,0 +1,6 @@
+/* Node.JS Javascript çalışma ortamında yarıçap değerini konsoldan parametre olarak girerek alanı bulan program.
+Konsol çıktısı: Yarıçapı (Yarıçap) olan dairenin alanı: (Alan) şeklinde olmalıdır. */
+
+//Process nesnesi node.js e ait olan global nesnelerden bir tanesidir ve bize o anki işlemle ilgili  bilgi verir.
+//Process in argv özelliği bize bir tane array döndü ve bu array programı çalıştırırken kullandığımız parametrelerdir.
+//
